8ee6bece4d4ba66e7999747be8bf6c7d549b62bb
admin <1395258209@qq.com>
2017-01-07 23:15:56 +0800

    创建工程

---
+++ AddAnimation/AddAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddAnimation
+//
+//  Created by admin on 17/1/7.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddAnimation/AddAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddAnimation
+//
+//  Created by admin on 17/1/7.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
